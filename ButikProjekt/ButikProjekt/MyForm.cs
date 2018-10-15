3d393bcc2eb3273e667925a085a9f33048a31631
Dennis <hammarstrand@hotmail.com>
2018-10-15 10:54:40 +0200

    Mainlayout update

--- ButikProjekt/ButikProjekt/MyForm.cs
+++ ButikProjekt/ButikProjekt/MyForm.cs
@@ -19,7 +19,7 @@ namespace ButikProjekt
         {
             Font = new Font("San Serif", 9f),
             ReadOnly = true,
-            ColumnCount = 2,
+            ColumnCount = 3,
             AutoSize = true,
             AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill,
             RowHeadersVisible = false,
@@ -30,6 +30,24 @@ namespace ButikProjekt
             AllowUserToResizeColumns = false,
             AllowUserToResizeRows = false
         };
+        private DataGridView ShoppingCartGridView = new DataGridView
+        {
+            Font = new Font("San Serif", 9f),
+            ReadOnly = true,
+            ColumnCount = 3,
+            AutoSize = true,
+            AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill,
+            RowHeadersVisible = false,
+            BackgroundColor = SystemColors.Control,
+            BorderStyle = BorderStyle.None,
+            AllowUserToAddRows = false,
+            AllowUserToDeleteRows = false,
+            AllowUserToResizeColumns = false,
+            AllowUserToResizeRows = false
+        };
+        private Button Add = new Button { Font = new Font("San Serif", 15f), Text = "Add", AutoSize = true, Dock = DockStyle.Top };
+        private Button Remove = new Button { Font = new Font("San Serif", 15f), Text = "Remove", AutoSize = true, Dock = DockStyle.Top };
+        private Button Buy = new Button { Font = new Font("San Serif", 15f), Text = "Buy", AutoSize = true, Dock = DockStyle.Top };
 
         public MyForm()
         {
@@ -41,8 +59,10 @@ namespace ButikProjekt
             MainLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33));
             Controls.Add(MainLayout);
             MainLayout.Controls.Add(ShopGridView);
-
-
+            MainLayout.Controls.Add(ShoppingCartGridView, 2, 0);
+            MainLayout.Controls.Add(Add, 1, 4);
+            MainLayout.Controls.Add(Remove, 1, 5);
+            MainLayout.Controls.Add(Buy, 1, 6);
 
         }
     }
